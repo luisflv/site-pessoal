5fb96c7016bf48b7faccdf2d3ddd554803a54a25
luisflv <luisflv@gmail.com>
2024-02-11 10:04:13 -0300

    Fist commit

---
+++ SITE_ENTRA21/javascript.js
@@ -0,0 +1,13 @@
+function mensagem(){
+alert("Hello Malandro")
+}
+
+
+var vetorzao = [1, "Luís", 31.5];
+
+function mostrarVet(){
+    for (var i = 0; i < 3; i++){
+        alert(vetorzao[i])
+    }
+    alert(vetorzao[0] + vetorzao[1] + vetorzao[2])
+}
